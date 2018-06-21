8f86844c28f5e5f9c5ff01fd723d0a6dc056e416
Atanas Chachev <atanas@rubberduck.xyz>
2018-06-21 21:19:29 +0300

    Setting up the files structure , creating file bundling with webpack , sass and babel transpilers added

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+module.exports = {
+	entry: "./src/js/main.js",
+	output: {
+		path: path.resolve(__dirname , "dist"),
+		filename: "bundle.js",
+		publicPath: "/dist"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['es2015']
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader"
+					},
+					{
+						loader: "sass-loader"
+					}
+				]
+			}
+		]
+	},
+	watch: true
+};
