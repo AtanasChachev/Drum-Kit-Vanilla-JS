4f8e0cbb981a82ac83fc45fe28014d529c6a98f9
Atanas Chachev <atanas@rubberduck.xyz>
2018-06-26 10:19:26 +0300

    Adding the animations

--- src/js/animations_controller.js
+++ src/js/animations_controller.js
@@ -1,6 +1,46 @@
+import anime from 'animejs';
+
+// console.log(anime);
+
 // Animations will be initialized here
 export let animationController = {
 	init: () => {
-		console.log('animations init');
+		let animeTimeline = anime.timeline();
+
+		animeTimeline
+
+		.add({
+			targets: '.click--animation span',
+			opacity: '1',
+			duration: 1000,
+			delay: (target , index) => {
+				return index * 100;
+			}
+		})
+
+		.add({
+			targets: '.line',
+			width: '90px',
+			duration: 1000,
+			offset: '-=600'
+		})
+
+		.add({
+			targets: '.or--text',
+			scale: [
+				{ value: '0' , duration: 0 },
+				{ value: '1' , duration: 1000 }
+			],
+			duration: 1000
+		})
+
+		.add({
+			targets: '.tap--animation span',
+			opacity: '1',
+			duration: 1000,
+			delay: (target , index) => {
+				return index * 100;
+			}
+		})
 	}
 };
